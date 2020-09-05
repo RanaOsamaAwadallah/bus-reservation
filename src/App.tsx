438ec64a24bf57be6106496e54838ce14838a7e2
rosama@raisaenergy.com <rosama@raisaenergy.com>
2020-09-05 15:41:39 +0200

    [NEW] Added header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.css";
 import Map from "./components/map/map";
+import BusLogo from "./assets/bus.svg";
 
 function App() {
   return (
     <div className="App">
+      <header>
+        <img src={BusLogo} alt="Bus Logo" />
+      </header>
       <Map />
     </div>
   );
